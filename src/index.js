e0916ae4828442f1bde08c2a15543dcfb0d92fa3
Art <pytagpro@gmail.com>
2022-08-05 12:59:12 +0500

    fix: 13 test pass.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
     let tempStack = [];
-    for (let i of bracketsConfig) {
-        for (let n = 0; n < str.length; n++) {
-            if (str[n] === i[0]) {
+    for (let n = 0; n < str.length; n++) {
+        for (let i = 0; i < bracketsConfig.length; i++) {
+            if (str[n] === bracketsConfig[i][0]) {
                 tempStack.push(str[n]);
-            } else if (str[n] === i[1]) {
+            } else if (
+                bracketsConfig[i][1] === str[n] &&
+                bracketsConfig[i][0] === tempStack[tempStack.length - 1]
+            ) {
                 tempStack.pop();
             }
         }
     }
     return tempStack.length === 0 ? true : false;
 };
+
+// (i[0] === tempStack[tempStack.length - 1]) == i[0]
+// module.exports = function check(str, bracketsConfig) {
+//   let tempStack = [];
+//   for (let i of bracketsConfig) {
+//     for (let n = 0; n < str.length; n++) {
+//       if (str[n] === i[0]) {
+//         tempStack.push(str[n]);
+//       } else if (str[n] === i[1]) {
+//         tempStack.pop();
+//       }
+//     }
+//   }
+//   return tempStack.length === 0 ? true : false;
+// };
