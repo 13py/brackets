2865a6ecdd691e72aee0589dd76de9360e479001
Art <pytagpro@gmail.com>
2022-08-05 11:30:33 +0500

    feat: add test solve.(work but not fully).

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let tempStack = [];
+    for (i of bracketsConfig) {
+        for (let n = 0; n < str.length; n++) {
+            if (str[n] === i[0]) {
+                tempStack.push(str[n]);
+            } else if (str[n] === i[0]) {
+                tempStack.pop();
+            }
+        }
+    }
+    return tempStack ? false : true;
+};
